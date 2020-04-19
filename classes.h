d900ff78ead3990969950f58ffaacc6d5451eda0
j-cardenas <jesus.cardenassalas@my.tccd.edu>
2020-04-19 03:53:12 +0000

    Initial commit

---
+++ classes.h
@@ -0,0 +1,105 @@
+#include <iostream>
+#include <unistd.h> //for sleep command
+#include <string>
+#include <iomanip> //for setw
+using namespace std; 
+
+class Team
+{
+  private: 
+    int score; 
+    int fouls; 
+    string name; 
+    bool bonusFirstHalf;
+    bool bonusSecondHalf; 
+  public: 
+    Team()//default constructor  
+    {
+      score = 0;
+      fouls = 0; 
+      name = "";
+      bonusFirstHalf = false; 
+      bonusSecondHalf = false; 
+    }  
+    void setScore(int s) { score = s; }
+    void setFouls(int f) { fouls = f; }
+    void setName(string n) { name = n; }
+    void setBonusFirstHalf(bool bFH) { bonusFirstHalf = bFH; }
+    void setBonusSecondHalf(bool bSH) { bonusSecondHalf = bSH; }
+    double getScore() const { return score; }
+    double getFouls() const { return fouls; }
+    string getName() const { return name; }
+    bool getBonusFirstHalf() const { return bonusFirstHalf; }
+    bool getBonusSecondHalf() const { return bonusSecondHalf; }
+};
+
+class Scoreboard
+{
+  private: 
+    int period;
+    bool poss; //true = home, false = visitor 
+    Team team1; //double dot notation to get its data
+    Team team2; //double dot notation to get its data
+    int playerNumber;
+    int playerFouls; 
+    int minutes;
+    int seconds; 
+  public:
+    Scoreboard() //default constructor 
+    {
+      period = 1;
+      poss = true; //home team has the ball intially 
+      playerNumber = 0;
+      playerFouls = 0; 
+      minutes = 11;
+      seconds = 59; 
+    }  
+    void setPeriod(int p) { period = p; }
+    void setPoss(bool po) { poss = po; }
+    void setTeam1(Team t1) { team1 = t1; } //remember when the object changes
+    void setTeam2(Team t2) {team2 = t2; } //remember when the object changes
+    void setPlayerNumber(int pn) { playerNumber = pn; }
+    void setPlayerFouls(int pf) { playerFouls = pf; }
+    void setMinutes(int m) { minutes = m; }
+    void setSeconds(int s) { seconds = s; }
+    int getPeriod() const { return period; }
+    bool getPoss() const { return poss; }
+    Team getTeam1() const { return team1; }
+    Team getTeam2()  const { return team2; }
+    int getPlayerNumber() const { return playerNumber; }
+    int getPlayerFouls() const { return playerFouls; }
+    int getMinutes() const { return minutes; }
+    int getSeconds() const { return seconds; }
+    
+    void showScoreboard()
+    { 
+      string color = "\x1b[33;1m";
+      string reset = "\x1b[0m";
+        
+        system("clear");
+        
+        
+        //Show Scoreboard
+        for(int i = 0; i < 45; i++) { cout << "_"; }
+        cout << endl; 
+        cout << color << "\n\t\t2020 Corona Virus Bowl\n"<< reset;
+        for(int i = 0; i < 45; i++) { cout << "_"; }
+        cout << endl; 
+        cout << "     (H)\t\t\t\t\t\t(V)" << endl; 
+        cout << setw(10) << team1.getName() << "\t\t\t\t" << setw(12) << team2.getName()<< endl;
+        color = "\x1b[91;1m"; //red 
+        cout << color << setw(7) << team1.getScore() << "\t\t\t\t\t\t" << setw(6) <<  team2.getScore()  << reset<< endl;
+        cout << "Period: " << period << endl; 
+
+        //dealing with Boolean data
+        if(poss == true)
+        {
+          cout << team1.getName()<<" have the ball" << endl; 
+        }
+        else
+        {
+          cout << team2.getName()<<" have the ball" << endl; 
+        }
+      for(int i = 0; i < 45; i++) { cout << "_"; }
+    }
+};
---
+++ main.cpp
@@ -0,0 +1,110 @@
+/*
+Author: Jesus Cardenas
+Date: 4/18/2020
+Class: COSC-1437
+Sensei: Dr_T
+*/
+
+#include "classes.h" 
+#include <iostream>
+
+using namespace std; 
+
+
+
+
+ //A void function to instantiate and control scoreboard data 
+void scoreboardControls()
+{
+  Scoreboard s; //instantiate the the scoreboard object s
+  Team tOneMain; //team Object that will become a member of scoreboard S
+  Team tTwoMain; //team Object that will become a member of scoreboard S
+  char decision = '\0';
+  int newScore = 0; 
+  string newName = ""; 
+  string orange = "\x1b[33;1m"; //Orange
+  string red = "\x1b[91;1m"; //red 
+  string reset = "\x1b[0m";
+  s.setTeam1(tOneMain); // placed update Team1 object in s
+  s.setTeam1(tTwoMain); // placed update Team2 object in s
+
+system ("clear");
+cout <<orange<< "\t\t\tWelcome to a new experience\n\n\n"<<reset;
+
+sleep(3);
+
+cout << "Who is the"<<orange<<" HOME"<<reset<<" team? > ";
+cin >> newName; 
+tOneMain.setName(newName);      
+cout << "Who is the"<<orange<<" VISITOR"<<reset<<" team? > ";
+cin >> newName; 
+tTwoMain.setName(newName);
+cout <<red<< "\nConnecting to server..."<< endl;
+sleep(3); //pause 3 seconds.
+
+system ("clear");
+
+cout<< "Testing your internet connection... "<<reset<<endl;
+
+sleep(8);
+
+
+s.setTeam1(tOneMain); //setTeam1 to the latest data 
+s.setTeam2(tTwoMain); //setTeam2 to the latest data 
+
+
+  //work with the scoreboard and menu 
+  do
+  {
+        //show the scoreboard and clear the screen each time 
+        s.showScoreboard();
+        
+       
+       //Menu options for the user 
+        cout << "\nMenu: " << endl; 
+        cout << "A. Update "<<tOneMain.getName()<<" score" << endl; 
+        cout << "B. Update "<<tTwoMain.getName()<<" score" << endl; 
+        cout << "X. To Exit" << endl; 
+        cin >> decision; 
+
+        if(decision == 'a' || decision == 'A')
+        {
+          cout << "What is the new score? > ";
+          cin >> newScore; 
+          tOneMain.setScore(newScore);
+          cout << "\nConnecting to server..."<< endl;
+          sleep(3); //pause 3 seconds. 
+        
+        }
+        else if(decision == 'b' || decision == 'B')
+        {
+          cout << "What is the new score? > ";
+          cin >> newScore; 
+          tTwoMain.setScore(newScore);
+          cout << "\nConnecting to server..."<< endl;
+          sleep(3); //pause 3 seconds. 
+        
+        }
+        else if(decision == 'x' || decision == 'X')
+        {
+          cout << "\nEXIT. Go Mavs!!!" << endl; 
+        }
+
+        s.setTeam1(tOneMain); //setTeam1 to the latest data 
+        s.setTeam2(tTwoMain); //setTeam2 to the latest data 
+      
+  }while(decision != 'x' && decision != 'X');     
+
+
+}
+
+int main() 
+{
+
+  
+ 
+   
+   scoreboardControls(); //run the scoreboard processes and menus
+
+  return 0; 
+}
